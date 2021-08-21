7f9885e730111a833faaa09653c774a5fd2bd24d
manish-mp <56396260+manish-mp@users.noreply.github.com>
2021-08-21 16:15:58 +0530

    Day3 Assignment

---
+++ Day3Assignment/src/Footer.js
@@ -0,0 +1,9 @@
+function Footer() {
+	return (
+		<div className="Footer">
+			<code>Copyright (c) 2021</code>
+		</div>
+	);
+};
+
+export default Footer;
---
+++ Day3Assignment/src/Header.js
@@ -0,0 +1,9 @@
+function Header() {
+	return (
+		<div className="header">
+			<p>Movies Info</p>
+		</div>
+	);
+};
+
+export default Header;
